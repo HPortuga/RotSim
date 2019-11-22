4834356626541effeda6d78d3cf498b33ff58a66
Matheus Soares <mathsoares1@gmail.com>
2019-11-22 14:59:55 -0400

    + tarefas

--- emissor.py
+++ emissor.py
@@ -1,2 +1,12 @@
 class emissor():
-  pass
+  def __init__(self, ipRoteador, portaRoteador, ipOrigem, ipDestino, mensagem):
+    self.ipRoteador = ipRoteador
+    self.portaRoteador = portaRoteador
+    self.ipOrigem = ipOrigem
+    self.ipDestino = ipDestino
+    self.mensagem = mensagem
+
+    self.cabecalho = self.construirCabecalho()
+
+  def constrirCabecalho(self):
+    return dict()
