5fb744957dc03bb571ac597f74388d8fc4452e07
padilhaClaudio <padilha86@gmail.com>
2019-11-30 15:40:05 -0400

    ...

--- roteador.py
+++ roteador.py
@@ -1,8 +1,8 @@
 import socket
 
 class roteador():
-  def __init__(self, porta, tabRot):
-    self.localIP = "127.0.0.1"
+  def __init__(self, porta, tabRot, meuIp):
+    self.localIP = meuIp.strip(".")
     self.bufferSize = 65335                           # Max packet size
     self.porta = porta
     self.tabRot = tabRot                              # Pode ser uma lista de dicionarios construida na main a partir do txt. Os dicionarios possuem rede-destino/máscara/gateway/interface
@@ -10,26 +10,35 @@ class roteador():
 
   def startServer(self):
     updServerSocket = socket.socket(family=socket.AF_INET, type= socket.SOCK_DGRAM)
-    updServerSocket.bind((self.localIP, self.porta))
+    updServerSocket.bind(("127.0.0.1", self.porta))
     print("Servidor UDP escutando...")
 
     while(True):
       data = updServerSocket.recvfrom(self.bufferSize)
-      pkg = list(data[0])
-      address = data[1]
-      ttl = pkg[8]
-      origem = pkg[12:16]
-      destino = pkg[16:20]
-      msg = "".join(pkg[24:])
+      self.processarPacote(bytearray(data[0]))
 
-  def processarPacote (self, cabecalho):
-    
-    if cabecalho["TTL"] == 1:                         # Dropa pacote caso TTL tenha acabado
-      print("Time to Live exceeded in Transit, dropping packet for" + cabecalho["destino"])
+  def processarPacote (self, data):
+    pkg = list(data)
+    ttl = pkg[8]
+    origem = pkg[12:16]
+    destino = pkg[16:20]
+    msg = pkg[24:]
+
+    for i in range(len(msg)):
+      msg[i] = chr(msg[i])
+
+    msg = "".join(msg)
+           
+    if ttl == 1:                         # Dropa pacote caso TTL tenha acabado
+      print("Time to Live exceeded in Transit, dropping packet for ".join(destino))
       return
 
-    cabecalho["TTL"] -= 1
+    data[8] = ttl - 1
+
+    for e in self.tabRot:
+      e
+
 
     # TODO: Check the correct Route. Send packet if there is a route, or inform if the destination is not reachable
 
-roteador = roteador(8080, "")
+roteador = roteador(8080, "", "65.65.65.65")
