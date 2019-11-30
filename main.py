298a89c773f71cccd0edbfbd80cb87a1f7c91896
Matheus Soares <mathsoares1@gmail.com>
2019-11-30 15:39:29 -0400

    Executando cada servico num terminal

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import os
+
 if __name__ == "__main__":
-    # Usar SocketServer
+
+	# Abre uma janela para cada processo
+	os.system("gnome-terminal -x python3 roteador.py")
+	os.system("gnome-terminal -x python3 emissor.py")
